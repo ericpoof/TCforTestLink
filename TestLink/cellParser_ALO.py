99483b189cda9d5cd648b44e19a82da91643ebd8
ericpoof <ericpoof@gmail.com>
2013-08-01 09:08:44 -0700

    debugging parsing routine and custom mardown file.

--- TestLink/cellParser_ALO.py
+++ TestLink/cellParser_ALO.py
@@ -17,6 +17,9 @@ from collections import deque
 class CellParser(object):
     def __init__(self):
         self.content = []
+        self.noTC = 0
+        self.noCont = 0
+        self.noData = 0
 
     def readTextMD(self, file):
         """
@@ -46,18 +49,6 @@ class CellParser(object):
         ### testcases
         ts.testcases = []
 
-        ## Looping in rows
-        for row in self.content:
-            self.mapRow2TC(row)
-
-#         testsuites = [ts]
-
-        ## The end result before passing to #3 XmlTree converter
-#         return testsuites                        
-                        
-                        
-    def mapRow2TC(self, row):
-
         re_title = re.compile(reAL.Title, re.I)
         re_steps = re.compile(reAL.Test_Step, re.I)
         re_cont = re.compile(reAL.Continue, re.I)
@@ -66,86 +57,135 @@ class CellParser(object):
         re_ex = re.compile(reAL.EX, re.I)
         re_cr = re.compile(reAL.CR, re.I)
         re_none = re.compile(reAL.NoData, re.I)
-        
+        nline = reAL.Newline
+
+        ## Looping in rows
         steps_queue = deque('',maxlen=15)
         data_queue = deque('',maxlen=15)
         expt_queue = deque('',maxlen=15)
-        
-        nline = reAL.Newline
         
         flag_tc = False
         flag_st = False
         flag_dt = False
         flag_ex = False
 
-        if re_title.match(row):            # QE
-            print '=> title row'
-            tc = TestCase()
-            tc.title = row
-            flag_ex = False
-            flag_tc = True
-            st_no = 0
-        elif re_ts.match(row):             # Test Step 
-            print '=> Test Step'
-            flag_tc = False
-            flag_st = True
-        elif re_steps.match(row):          # 1 xxx 
-            print '=> steps row'
-            steps_queue.append(row)
-        elif re_td.match(row):             # Test Data 
-            print '=> Test Data'
-            flag_st = False
-            flag_dt = True
-        elif re_cont.match(row):           # *xxx
-            print '=> continued'
-            if flag_st:
-                contd = steps_queue.pop() + row
-                steps_queue.append(contd)
-            if flag_dt:
-                contd = data_queue.pop() + row
-                data_queue.append(contd)
-            if flag_ex:
-                contd = expt_queue.pop() + row
-                expt_queue.append(contd)
-        elif re_none.match(row):           # @NoData 
-            print '=> no data'
-            if flag_dt:
-                data_queue.append('')
-            if flag_ex:
-                expt_queue.append('')
-        elif re_ex.match(row):             # Expected Result 
-            print '=> Expected Result'
-            flag_dt = False
-            flag_ex = True
-        elif re_cr.match(row):             # \n 
-            print '=> newline'
-        else:                              # item for data/expected
-            print '=> data/expected item'
-            if flag_tc:
-                tc.title = tc.title + row
-            if flag_dt:
-                data_queue.append(row)
-            if flag_ex:
-                expt_queue.append(row)
-
-            # The end of testcase
-            if flag_ex and (len(data_queue) == len(expt_queue)):
+        for i in range(8, len(self.content)):
+            row = self.content[i]
+#             self.mapRow2TC(row, steps_queue, data_queue, expt_queue)
+            print ' -------'
+            print ': Row = ' , row
+            if re_title.match(row):            # QE
+                print '=> title row'
+                tc = TestCase()
+                tc.title = row
                 flag_ex = False
+                flag_tc = True
+                st_no = 0
+            elif re_ts.match(row):             # Test Step 
+                print '=> Test Step'
+                flag_tc = False
+                flag_st = True
+            elif re_steps.match(row):          # 1 xxx 
+                print '=> steps row'
+                steps_queue.append(row)
+            elif re_td.match(row):             # Test Data 
+                print '=> Test Data'
+                flag_st = False
+                flag_dt = True
+            elif re_cont.match(row):           # *xxx
+                print '=> continued'
+                if flag_st:
+                    contd = steps_queue.pop() + row
+                    steps_queue.append(contd)
+                if flag_dt:
+                    contd = data_queue.pop() + row
+                    data_queue.append(contd)
+                if flag_ex:
+                    contd = expt_queue.pop() + row
+                    expt_queue.append(contd)
+            elif re_none.match(row):           # @NoData 
+                print '=> no data'
+                if flag_dt:
+                    data_queue.append('')
+                if flag_ex:
+                    expt_queue.append('')
+#                 self.createStep(flag_ex, steps_queue, data_queue, expt_queue)
+            elif re_ex.match(row):             # Expected Result 
+                print '=> Expected Result'
+                flag_dt = False
+                flag_ex = True
+            elif re_cr.match(row):             # \n 
+                print '=> newline'
+#                 self.createStep(flag_ex, steps_queue, data_queue, expt_queue)
+                # The end of testcase
+#                 if flag_ex and (len(data_queue) == len(expt_queue)):
+#                     print '============= testcase ended====================='
+#                     flag_ex = False
+#                     # sanity check
+#                     if len(steps_queue) != len(data_queue):
+#                         print 'Mismatch in steps and data'
+#                         print 'len steps_queue', len(steps_queue)
+#                         print 'len data_queue', len(data_queue)
+#                     # Need to combine steps_queue and data_queue
+#                     for i in range(len(steps_queue)):
+#                         st = Step()
+#                         st.actions = steps_queue.popleft() + ':' + data_queue.popleft()
+#                         print '--- actions = ', st.actions
+#                         st.expectedresults = expt_queue.popleft()
+#                         print '---Expt = ', st.expectedresults
+#                     
+#                     self.noTC = self.noTC + 1
+#                     self.noData = self.noData + 1
+#                     self.noCont = self.noCont + 1
+#                     print 'noTC from noData', self.noData
+#                     print 'noTC from noCont', self.noCont
+#                     print 'Total testcases = ', self.noTC
+#                     steps_queue = deque('', maxlen=15)
+#                     data_queue = deque('', maxlen=15)
+#                     expt_queue = deque('', maxlen=15)
+            else:                              # item for data/expected
+                print '=> data/expected item'
+                if flag_tc:
+                    tc.title = tc.title + row
+                if flag_dt:
+                    data_queue.append(row)
+                if flag_ex:
+                    expt_queue.append(row)
+#                 self.createStep(flag_ex, steps_queue, data_queue, expt_queue)
+                if flag_ex and (len(data_queue) == len(expt_queue)):
+                    print '============= testcase ended====================='
+                    flag_ex = False
+                    # sanity check
+                    if len(steps_queue) != len(data_queue):
+                        print 'Mismatch in steps and data'
+                        print 'len steps_queue', len(steps_queue)
+                        print 'len data_queue', len(data_queue)
+                    # Need to combine steps_queue and data_queue
+                    for i in range(len(steps_queue)):
+                        st = Step()
+                        st.actions = steps_queue.popleft() + ':' + data_queue.popleft()
+                        print '--- actions = ', st.actions
+                        st.expectedresults = expt_queue.popleft()
+                        print '---Expt = ', st.expectedresults
+                    
+                    self.noTC = self.noTC + 1
+                    self.noData = self.noData + 1
+                    self.noCont = self.noCont + 1
+                    print 'noTC from noData', self.noData
+                    print 'noTC from noCont', self.noCont
+                    print 'Total testcases = ', self.noTC
+                    steps_queue = deque('', maxlen=15)
+                    data_queue = deque('', maxlen=15)
+                    expt_queue = deque('', maxlen=15)
+             
 
-                # sanity check
-                if len(steps_queue) != len(data_queue):
-                    print 'Mismatch in steps and data'
+#         testsuites = [ts]
 
-                # Need to combine steps_queue and data_queue
-                for i in range(len(steps_queue)):
-                    st = Step()
-                    st.actions = steps_queue.popleft() + ':' + data_queue.popleft()
-                    print '--- actions = ', st.actions
-                    st.expectedresults = expt_queue.popleft()
-                    print '---Expt = ', st.expectedresults
-
-                
-
+        ## The end result before passing to #3 XmlTree converter
+#         return testsuites                        
+                        
+                        
+#     def mapRow2TC(self, row, steps_queue, data_queue, expt_queue):
 
 
     def printTestSuites(self, testsuites):
@@ -160,3 +200,4 @@ class CellParser(object):
                     print 'step=', step.step_number, ' :' , step.actions
                     if step.step_number == len(tc.steps):
                         print 'expected =', step.expectedresults
+
